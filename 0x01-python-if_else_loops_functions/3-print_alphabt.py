1b1d68db508a7050cc149bf5ff9a135ed9eae09c
MohammedMaghraoui <mohamdmaghraoui99@gmail.com>
2023-10-03 12:05:37 -0700

    When I was having that alphabet soup

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for Y in range(97, 123):
-    if char(Y) is not 'q' and char(Y) is not 'e'
+    if chr(Y) is not 'q' and chr(Y) is not 'e':
         print("{}".format(chr(Y)), end="")
