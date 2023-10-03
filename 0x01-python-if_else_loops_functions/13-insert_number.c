3fee5d02732f7d9cfd477668f4fb08fcf9f70a8f
MohammedMaghraoui <mohamdmaghraoui99@gmail.com>
2023-10-03 15:44:57 -0700

    Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,47 +1,34 @@
 #include "lists.h"
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
- * insert_node- insert a node into a sorted linked list
- * @head: the head of the linked list
- * @number: is the number inserted
- * Return: the linked list address, after insertion
+ * insert_node - Inserts a number into a sorted singly-linked list.
+ * @head: A pointer the head of the linked list.
+ * @number: The number to insert.
+ *
+ * Return: If the function fails - NULL.
+ * Otherwise - a pointer to the new node.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	struct listint_s *node = malloc(sizeof(listint_t));
-	struct listint_s *tracker = *head;
-	struct listint_s *step_back = NULL;
+	listint_t *node = *head, *new;
 
-	if ((*head)->n > number)
-	{
-		node->next = (*head);
-		return (node);
-	}
-	node->n = number;
-	node->next = NULL;
-	/*if head is null , return null*/
-	if ((*head) == NULL)
-		return (node);
-	/*finding the spot where number < tracker->n*/
-	while (tracker->n < number && tracker != NULL)
-	{
-		step_back = tracker;
-		tracker = tracker->next;
-	}
-	/*if tracker null , then there is no bigger element,add last*/
-	if (tracker == NULL)
-	{
-		add_nodeint_end(head, number);
-	}
-	/*if tacker != null, then a bigger element must have been found*/
-	/*insert it*/
-	else
-	{
-		node->next = tracker;
-		step_back->next = node;
-	}
-	return (*head);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
 
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
 }
