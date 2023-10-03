175f4f67e0d0589af74e223de82fdc51cf4e851c
MohammedMaghraoui <mohamdmaghraoui99@gmail.com>
2023-10-03 11:54:17 -0700

    I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # 2-print_alphabet.py
-***prints the ASCII alphabet, in lowercase, not followed by a new line.***
+"""Print the alphabet in lowercase, not followed by a new line."""
     for int in range(48, 96):
         print("{:d}".format(int), end='')
