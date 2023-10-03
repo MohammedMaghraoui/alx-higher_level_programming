112e3a8dff423bb46ee67ff15fd07e7bcf59d67d
MohammedMaghraoui <mohamdmaghraoui99@gmail.com>
2023-10-03 11:39:03 -0700

    I play what I call the alphabet game

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 import random
+***prints the ASCII alphabet, in lowercase, not followed by a new line.***
+    for int in range(48, 96):
+        print("{:d}".format(int), end='')
