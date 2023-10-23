0bd4ed34a9f55313b23b31d2698cb557343776e6
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2023-10-23 09:27:12 -0700

    Safe printing of an integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,23 +1,8 @@
 #!/usr/bin/python3
-safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
-def check_and_print_integer(value):
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-
-value = 89
-check_and_print_integer(value)
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-value = -89
-check_and_print_integer(value)
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
-value = "School"
-check_and_print_integer(value)
-    has_been_print = safe_print_integer(value)
-    if not has_been_print:
-        print("{} is not an integer".format(value))
+def safe_print_integer(value):
+    try:
+        print(int(value))
+        return True
+    except (ValueError, TypeError):
+        return False
