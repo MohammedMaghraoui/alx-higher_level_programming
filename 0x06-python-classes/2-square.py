e199d75ba707b264cc38c1e2f00bca1ed937e059
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2023-10-24 08:46:00 -0700

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,19 @@
+#!/usr/bin/python3
+"""Square module."""
+
 class Square:
+    """Defines a square."""
+
     def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size: length of a sde of the square.
+
+        Raises:
+            typeError: if size is not an integer
+            ValueError: if size is less than 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
